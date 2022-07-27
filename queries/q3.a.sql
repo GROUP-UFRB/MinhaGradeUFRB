6e26e3f5c9725fa562163ef07098feeb244f3cb7
Joel Pires <joelpires70@gmail.com>
2022-07-26 21:27:50 -0300

    create q3.a.sql

---
+++ queries/q3.a.sql
@@ -0,0 +1,17 @@
+/*
+Query: Quais as matérias de maior peso do curso?
+*/
+
+/*
+TODO assert provide student_id
+*/
+select sub.subject_id, sub.name, sub.cod_center, sub.weight, sub.workload 
+
+    from minhagrade.Subject sub, minhagrade.Course c, minhagrade.Student s,
+    minhagrade.CourseRequireSubject c_r_s
+
+        where
+            s.student_id        = student_id
+            and c_r_s.course_id = s.course_id
+            and sub.subject_id  = c_r_s.subject_id
+            and sub.sub_weight  = max(sub.weight)
