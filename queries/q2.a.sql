a29ab6571f3ade3fd4725f49a50f3b1ee797f9cf
crocha12 <camilorocha11@gmail.com>
2022-07-28 14:39:32 -0300

    add querie q2.c.sql

--- queries/q2.a.sql
+++ queries/q2.a.sql
@@ -1,5 +1,5 @@
 /*Query: "Quais matérias já foram pegas pelo aluno?" */
-/*TODO assert we have register: to specific student */
+/*TODO assert we have subject_id: to specific student */
 
 select * from minhagrade.SubjectStudent as s_s
 where s_s.subject_id = subject_id
