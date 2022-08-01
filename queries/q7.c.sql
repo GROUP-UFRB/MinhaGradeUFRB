0ed5124615a97bf850f06a8fc9d02a4d8276c4cd
crocha12 <camilorocha11@gmail.com>
2022-08-01 20:30:43 -0300

    add query q7.b-e

---
+++ queries/q7.c.sql
@@ -0,0 +1,25 @@
+/*Com as matérias já executadas pelo aluno neste curso, 
+ qual a porcentagem de carga horária complementar para o aluno executada em relação ao total?*/
+with carga_horaria_complementar_curso as (
+    SELECT
+        "workload_S_complement" as carga_horaria_complementar_curso
+    FROM
+        "Course" co
+    WHERE
+        co.cod_course = 'BCET'
+),
+carga_horaria_complementar_aluno as (
+    SELECT
+        sum(ca.value) as carga_horaria_complementar_aluno
+    FROM
+        "ComplementActivity" ca
+    WHERE
+        ca.student_id = 2
+)
+SELECT
+    (
+        cast(carga_horaria_complementar_aluno as float8) / carga_horaria_complementar_curso
+    ) as porcentagem_carga_horaria_complementar
+FROM
+    carga_horaria_complementar_curso,
+    carga_horaria_complementar_aluno
