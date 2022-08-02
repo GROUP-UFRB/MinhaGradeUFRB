d2008b43710fb46d887f8cff9408978ad6781f62
crocha12 <camilorocha11@gmail.com>
2022-08-02 10:15:05 -0300

    update query q6.a.sql

--- queries/q6.a.sql
+++ queries/q6.a.sql
@@ -1,21 +1,39 @@
 /*
-Query: Qual o índice de aprovação de determinada matéria?
-*/
-/*
-TODO assert provide subject_id, x
-*/
-with aproved_subjects as (
-    select 
-        count(s_s.student_id) as count_appr
-    from 
-        SubjectStudent s_s
-    where 
-        s_s.subject_id = subject_id and s_s.status = "Approved"
+ Query: Qual o índice de aprovação de determinada matéria?
+ */
+with resultados as (
+    SELECT
+        s.subject_code,
+        count(subject_student_id) as todos_resultados
+    FROM
+        "CourseRequireSubject" crs
+        JOIN "Subject" s ON s.subject_id = crs.subject_id
+        JOIN "SubjectStudent" ss ON ss.subject_code = s.subject_code
+    WHERE
+        crs.cod_course = 'BCET'
+        and ss.subject_code = 'CCA310'
+        and ss.status != 'cursando'
+    GROUP BY
+        s.subject_code
+),
+aprovados as (
+    SELECT
+        s.subject_code,
+        count(subject_student_id) as todos_aprovados
+    FROM
+        "CourseRequireSubject" crs
+        JOIN "Subject" s ON s.subject_id = crs.subject_id
+        JOIN "SubjectStudent" ss ON ss.subject_code = s.subject_code
+    WHERE
+        crs.cod_course = 'BCET'
+        and ss.subject_code = 'CCA310'
+        and ss.status = 'aprovado'
+    GROUP BY
+        s.subject_code
 )
-
-select 
-    count_appr/count(s_s.student_id) as indice_aprovacao 
-from 
-    SubjectStudent s_s
-where 
-    s_s.subject_id = subject_id
+SELECT
+    re.subject_code,
+    (cast(todos_aprovados as float) / todos_resultados) as indice_aprovacao
+FROM
+    "resultados" re,
+    "aprovados" ap
